4d5b1b0ce690b304802e86782c946b26e4d5f8f4
Nathan <nathan.overschie@gmail.com>
2022-09-22 22:29:16 +0200

    first commit

--- src/FormulaManipulator.hs
+++ src/FormulaManipulator.hs
@@ -1,25 +1,34 @@
-{-|
-Module      : FormulaManipulator
-Description : Manipulate formulas and expressions represented by `Expr` values
-Copyright   : STUDENT NAME 1 (ID)
-              STUDENT NAME 2 (ID)
-
-`FormulaManipulator` offers functions to manipulate, evaluate, and print
-formulas and expressions represented by `Expr` values.
--}
+-- |
+-- Module      : FormulaManipulator
+-- Description : Manipulate formulas and expressions represented by `Expr` values
+-- Copyright   : STUDENT NAME 1 (ID)
+--               STUDENT NAME 2 (ID)
+--
+-- `FormulaManipulator` offers functions to manipulate, evaluate, and print
+-- formulas and expressions represented by `Expr` values.
 module FormulaManipulator
-  ( foldE
-  , printE
-  , evalE
-  , simplifyE
-  , diffE
-  )
+    (   foldE,
+        printE,
+        evalE,
+        simplifyE,
+        diffE,
+    )
 where
 
-import           ExprLanguage                   ( Expr(Var, Const, Plus, Mult) )
+import ExprLanguage (Expr (Const, Mult, Plus, Var))
 
-foldE     = error "Implement, document, and test this function"
-printE    = error "Implement, document, and test this function"
-evalE     = error "Implement, document, and test this function"
+foldE :: p -> (Expr t t -> Expr t t -> p) -> Expr t t -> p
+foldE b g = rec
+    where
+        rec (Var x) = b
+        rec (Const y) = b
+        rec (Mult x y) = g x y
+        rec (Plus x y) = g x y
+
+printE = error "Implement, document, and test this function"
+
+evalE = error "Implement, document, and test this function"
+
 simplifyE = error "Implement, document, and test this function"
-diffE     = error "Implement, document, and test this function"
+
+diffE = error "Implement, document, and test this function"
